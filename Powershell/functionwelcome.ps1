3a164af8718aac22b764dd45d1c77f5081d7e0f8
unknown <200446961@student.georgianc.on.ca>
2021-04-07 23:58:26 -0700

    Create functionwelcome.ps1

---
+++ Powershell/functionwelcome.ps1
@@ -0,0 +1,14 @@
+﻿function welcome { 
+"Welcome to Powershell"
+write-output "Welcome to planet $env:computername Overlord $env:username"
+$now = get-date -format 'HH:MM tt on dddd'
+write-output "It is $now."
+}
+
+function get-cpuinfo {
+Get-CimInstance cim_processor | Format-List "Manufacturer", "Name", "MaxClockSpeed", "NumberofCrores"
+}
+
+function get-mydisks {
+Get-Disk | Format-Table Manufacturer, Model, SerialNumber, FirmwareRevision, Size
+}
