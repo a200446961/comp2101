71f80163de73d5aac5ab4e4b74629c8154e60fb5
unknown <200446961@student.georgianc.on.ca>
2021-04-07 23:55:52 -0700

    Create lab3-ipconfig.ps1

---
+++ Powershell/lab3-ipconfig.ps1
@@ -0,0 +1,5 @@
+﻿Get-CimInstance Win32_NetworkAdapterConfiguration | Where-Object IPEnabled -EQ True | Select-Object Description, Index, 
+@{n="IPAddress";e={$_.ipaddress}},
+@{n="Subnet Mask";e={$_.IPSubnet}},
+@{n="DNS Domain Name";e={$_.DNSHostName}},
+@{n="DNS Server";e={$_.DNSServerSearchOrder}} | Format-Table
