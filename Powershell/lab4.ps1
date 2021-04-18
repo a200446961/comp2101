f565fc9ba931f2ec7c299b1307bb3401804a710d
unknown <200446961@student.georgianc.on.ca>
2021-04-17 17:19:00 -0700

    Create lab4.ps1

---
+++ Powershell/lab4.ps1
@@ -0,0 +1,6 @@
+﻿#Netwrk Conf
+Get-CimInstance Win32_NetworkAdapterConfiguration | Where-Object IPEnabled -EQ True | Select-Object Description, Index, 
+@{n="IPAddress";e={$_.ipaddress}},
+@{n="Subnet Mask";e={$_.IPSubnet}},
+@{n="DNS Domain Name";e={$_.DNSHostName}},
+@{n="DNS Server";e={$_.DNSServerSearchOrder}} | Format-Table
